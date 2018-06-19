d4d1f53864dc41972856952883e0d574bd4758c2
Steven Imus <imussg@gmail.com>
2018-06-19 12:05:26 -0400

    solution

--- src/components/surprise.js
+++ src/components/surprise.js
@@ -6,10 +6,23 @@ import SurpriseImage from './surprise-image';
 export default class Surprise extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+        	displayButton: true
+        };
+    }
+
+    toggleState() {
+    	this.setState({
+    		displayButton: !this.state.displayButton
+    	});
     }
 
     render() {
         // Show the button to start with
-        return <SurpriseButton />;
+        if(this.state.displayButton) {
+        	return <SurpriseButton onClick={() => this.toggleState()}/>;
+        } else {
+        	return <SurpriseImage />;
+        }
     }
 }
